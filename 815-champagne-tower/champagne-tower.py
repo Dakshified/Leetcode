a24c2f757fd859a8ca3d16ac12689d29dd312cc5
Daksh Bhardwaj <bhardwajdakshcodes@gmail.com>
2026-07-12 21:39:22 +0530

    Time: 79 ms (69.97%) | Memory: 19.5 MB (38.17%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.py
@@ -0,0 +1,13 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        dp = [[0.0] * (r + 1) for r in range(102)]
+        dp[0][0] = poured
+
+        for r in range(query_row + 1):
+            for c in range(r + 1):
+                if dp[r][c] > 1:
+                    excess = (dp[r][c] - 1) / 2.0
+                    dp[r + 1][c] += excess
+                    dp[r + 1][c + 1] += excess
+
+        return min(1.0, dp[query_row][query_glass])
